4c28614aa083710fd55908bbe3f1b86c311a8a02
Oscar <oscar.abacka@gmail.com>
2018-12-01 14:49:45 +0200

    Commit

---
+++ LSTM.py
@@ -0,0 +1,188 @@
+import keras
+import matplotlib.pyplot as plt 
+import numpy as np 
+import os
+import pandas as pd 
+import re
+import sklearn.metrics
+import sys
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM
+from keras.optimizers import RMSprop
+
+# Data source: https://en.ilmatieteenlaitos.fi/download-observations#!/
+
+DATA_DIR = "data"
+
+def load_data(name, rename=True):
+    path = os.path.join(DATA_DIR, name)
+    df = pd.read_excel(path, index_col='Date')
+    
+    if rename:
+        abr = {'Pressure': 'P', 'Wind_speed': 'Wnd_s', 
+               'Air_temperature':'Tmp', 'Wind_direction':'Wnd_dir', 
+               'Cloud_amount': 'Clouds'}
+        df = df.rename(abr, axis='columns')
+    return df
+
+
+def build_dataframe(locations, features, features_d, target, steps):
+    dfs = []
+    for location in locations:
+        df_tmp = load_data(location + '.xlsx')
+        dfs.append(df_tmp)
+    df = pd.concat(dfs)
+    df = add_difference(df, features_d)
+
+    df = df[features_d + features]
+    df = add_prediction_column(df, target, steps)
+    return df.dropna(axis=0)
+    
+def add_difference(df, features, step=1):
+    for feature in features:
+        df[feature + '_d'] = df[feature].diff(step)
+    return df
+
+def add_prediction_column(df, feature, steps):
+    for step in steps:
+        df[feature + '_diff{}'.format(str(step))] = df[feature].diff(step).shift(-step)
+        df['target'] = df[feature].shift(-step)
+    return df
+
+
+def make_train_test(df, features, predictions, sequence_len, tr_size):
+    """Splits data into train and test."""
+    num_features = len(features)
+    num_predictions = len(predictions)
+    columns = features + predictions
+    df = df[columns].values
+    result = []
+    print('Number of features: {0}\nHorizon length: {1}'.format(num_features, sequence_len))
+    for index in range(len(df) - sequence_len + 1):
+        result.append(df[index: index + sequence_len])
+    result = np.array(result)
+    print('Shape of data: {}'.format(result.shape))
+    n = int(tr_size * result.shape[0])
+    X_test,y_test = None, None
+    X_train = result[:n, :, :-num_predictions]
+    y_train = result[:n, -1, -num_predictions:]
+    if tr_size < 1:
+        X_test = result[n:, :, :-num_predictions]
+        y_test = result[n:, -1, -num_predictions:]
+        print('X_train: {0}\ny_train: {1}\nX_test: {2}\ny_test: {3}'.format(X_train.shape, y_train.shape,
+                                                                       X_test.shape, y_test.shape))
+    return X_train, y_train, X_test, y_test, n
+
+def build_model(layers):
+    model = Sequential()
+    model.add(LSTM(
+            layers[1],
+            input_shape=(None, layers[0]),
+            return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(layers[2], return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(layers[3], return_sequences=False))
+    model.add(Dropout(0.2))
+    model.add(Dense(layers[4], activation="linear"))
+    model.compile(loss="mae", optimizer='rmsprop')
+    return model
+
+def train_model(model, X_train, y_train, epochs=20, batch_size=64):
+    print(model.summary())
+    history = model.fit(
+                X_train, y_train, 
+                batch_size=batch_size, 
+                epochs=epochs, 
+                validation_split=0.1,
+                shuffle = True,
+                callbacks = [keras.callbacks.ModelCheckpoint(model_path, monitor='loss', verbose=0, 
+                                                            save_best_only=True, save_weights_only=False, 
+                                                            mode='min', period=1
+                                                            ),
+                            keras.callbacks.EarlyStopping(monitor='loss', min_delta=0, 
+                                                          patience=6, verbose=1, mode='min', 
+                                                          )
+                            ]
+                )
+    return history
+
+def evaluate_predictions(predictions, true_values):
+    mean_abs_error = sklearn.metrics.mean_absolute_error(true_values, predictions)
+    mean_sqrd_error = sklearn.metrics.mean_squared_error(true_values, predictions)
+
+    print('Mean absolute error: {}'.format(mean_abs_error))
+    print('Mean squared error: {}'.format(mean_sqrd_error))
+    
+
+def run_training(data, model, features, target_d, look_back, train_size, epochs=10):
+    X_train, y_train, X_test, y_test, split_point = make_train_test(data, features, target_d, look_back, train_size)
+    
+    true_values = data['target'][split_point + look_back - 1:] #Future values we want to predict
+    orig_values = data[target][split_point + look_back - 1:]   #Values at t=0
+
+    history = train_model(model, X_train, y_train, epochs=epochs)
+    predicted_difference = model.predict(X_test).flatten() # Predicted differences
+    predictions = orig_values + predicted_difference
+    
+    print('*'*50, 'Test errors model', sep='\n')
+    evaluate_predictions(predictions, true_values)
+    print('*'*50, 'Test errors naive model', sep='\n')
+    evaluate_predictions(orig_values, true_values)
+    return history, predictions, true_values, predicted_difference
+
+def run_validation(df, features, target_d, look_back, tr_size=1, model=None):
+    """Tests a model on unseen data."""
+    reshaped_data = make_train_test(df, features, target_d, look_back, tr_size)
+    X_data, y_data = reshaped_data[0], reshaped_data[1]
+
+    if model is None:
+        model = keras.models.load_model('model.HDF5')
+
+    true_values = df['target'][look_back - 1:]
+    orig_values = df[target][look_back - 1:]
+
+    predicted_difference = model.predict(X_data).flatten()
+    predictions = orig_values + predicted_difference 
+    print('*'*30, 'Errors model, unseen data', sep='\n')
+    evaluate_predictions(predictions, true_values)
+    print('*'*30, 'Errors naive model, unseen data', sep='\n')
+    evaluate_predictions(orig_values, true_values)
+    
+    return predictions, true_values, predicted_difference
+    
+train_location = ['kuopio-maaninka_2016']#, 'kasko_2016']
+val_location = ['kuopio-maaninka_2017']
+epochs = 20
+train_size = 0.80
+features_d = ['Wnd_s', 'Tmp']       # features to differentiate
+features = ['Wnd_s_d', 'Tmp_d']     # features to use in model
+look_back = 6                       # 
+target = 'Wnd_s'                    # feature to predict
+horizons = [6]                      # how far in the future to predict
+target_d = [target + '_diff' + str(h) for h in horizons] # 
+model_path = 'model.hdf5'
+model_layers=[len(features), 40, 30, 40, len(horizons)] 
+plt_start = 200 # plots all timestamps if set to 'None'
+plt_end = 400
+
+data_train = build_dataframe(train_location, features, features_d, target, horizons)
+print(data_train.describe())
+print(data_train.head())
+
+model = build_model(model_layers)
+
+history_train, predictions_test, true_values_test, pred_diff_train = run_training(data_train, model, features, target_d, 
+                                                                   look_back, train_size, epochs)
+plot_predictions(predictions_test, true_values_test, plt_start,plt_end, 'Training '+train_location[0])
+
+data_val = build_dataframe(val_location, features, features_d, target, horizons)
+predictions_val, true_values_val, pred_diff_val = run_validation(data_val, features, target_d, look_back, tr_size=1, model=model)
+
+plot_predictions(predictions_val, true_values_val, plt_start,plt_end, 'Validation unseen data')
+plt.show()
+
+
+#model.save('model.HDF5')
+#model = keras.models.load_model('model.HDF5')
---
+++ examples/scripts/multiple_lags_model2.py
@@ -0,0 +1,147 @@
+from math import sqrt
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import os
+from utils import load_data, add_shifted_features
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.metrics import mean_squared_error
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.layers import LSTM
+
+DATA_DIR = 'data'
+loc_target = 'kuopio-maaninka_2017'
+loc_neighbors = [] #['kasko_2017', 'hango-russaro_2017', 'utsjoki-nuorgam_2017'] # leave empty when no neighbours in model
+has_winddir=False
+abr = {'Pressure': 'P', 'Wind_speed': 'Wnd_s', 'Air_temperature':'Tmp', 'Wind_direction':'Wnd_dir', 'Cloud_amount': 'Clouds'}
+
+features = ['P', 'Tmp', 'Wnd_s']
+target = 'Tmp'
+if 'Wnd_h' in features and 'Wnd_v' in features: # = Wind east-west, Wind north-south
+    has_winddir = True
+
+train_size = 0.9
+epochs = 100
+do_scaling = True
+steps = [6]
+
+plot_start = '2017-10-28 00:00:00'
+plot_end = '2017-11-01 00:00:00'
+
+n_features = len(features * (1 + len(loc_neighbors)))
+n_hours = len(steps)
+
+data_path = os.path.join(DATA_DIR, '{}.xlsx'.format(loc_target))
+df = load_data(data_path, rename_abr=abr)
+if has_winddir:
+    df['Wnd_v'] = np.sin(df['Wnd_dir'])
+    df['Wnd_h'] = np.cos(df['Wnd_dir'])
+df = add_shifted_features(df, features, steps)
+
+# Adding data for neighbors
+for n in loc_neighbors:
+    data_path = os.path.join(DATA_DIR, '{}.xlsx'.format(n))
+    df_neighbor = load_data(data_path, rename_abr=abr)
+    if has_winddir:
+        df_neighbor['Wnd_v'] = np.sin(df_neighbor['Wnd_dir'])
+        df_neighbor['Wnd_h'] = np.cos(df_neighbor['Wnd_dir'])
+    
+    df_neighbor = add_shifted_features(df_neighbor, features, steps)
+    df_neighbor = df_neighbor.drop(features, axis=1)
+    df = df.join(df_neighbor, on='Date', rsuffix='_' + n[:4])
+
+df = df.dropna()
+print(df.head(5), '\n')
+print(df.describe())
+df_y = df[target].to_frame()
+df_X = df.drop(features, axis=1)
+#if 'Wnd_dir' in features:
+#    df_X = df_X.drop(['Wnd_v', 'Wnd_h'])
+
+print('X data shape: {0}\n'.format(df_X.shape))
+print(df_X.head())
+print('Y data shape: {0}\n'.format(df_y.shape))
+print(df_y.head())
+
+# Scaling the data to (0,1)
+if do_scaling:
+    scaler = StandardScaler()
+    #scaler = MinMaxScaler(feature_range=(0, 1))
+    X_data = scaler.fit_transform(df_X.values)
+    y_data = scaler.fit_transform(df_y.values)
+else:
+    X_data = df_X.values
+    y_data = df_y.values
+
+# Splitting into train and test data
+n = int(train_size * len(X_data))
+X_train, X_test = X_data[:n, :], X_data[n:, :]
+y_train, y_test = y_data[:n], y_data[n:]
+
+# LSTM needs a 3-D shape
+X_train = X_train.reshape((X_train.shape[0], n_hours, n_features))
+X_test = X_test.reshape((X_test.shape[0], n_hours, n_features))
+
+print('X_train reshaped: ', X_train.shape)
+print('X_test reshape: ', X_test.shape)
+
+
+# Defining the model
+model = Sequential()
+model.add(LSTM(50, input_shape=(X_train.shape[1], X_train.shape[2]), return_sequences=True))
+#model.add(Dense(1))
+model.add(Dropout(0.2))
+
+model.add(LSTM(15, return_sequences=True))
+
+model.add(Dropout(0.2))
+    
+model.add(LSTM(20, return_sequences=False))
+model.add(Dropout(0.2))
+    
+model.add(Dense(1, activation="linear"))
+    
+model.compile(loss="mse", optimizer='rmsprop')
+    
+print(model.summary())
+
+#model.compile(loss='MSE', optimizer='rmsprop')
+# fit network
+history = model.fit(X_train, y_train, epochs=epochs, batch_size=72, validation_data=(X_test, y_test), verbose=1, shuffle=False)
+# plot history
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='test')
+plt.legend()
+plt.show()
+ 
+# # make a prediction
+yhat = model.predict(X_test)
+
+if do_scaling:
+    yhat = scaler.inverse_transform(yhat)
+
+y_test = y_test.reshape((len(y_test), 1))
+
+if do_scaling:
+    y_test = scaler.inverse_transform(y_test)
+
+# calculate RMSE
+rmse = sqrt(mean_squared_error(y_test, yhat))
+print('Test RMSE: %.3f' % rmse)
+
+y_test = pd.Series(y_test.flatten(), index = df_y.iloc[n:, :].index)
+yhat = pd.Series(yhat.flatten(), index = df_y.iloc[n:, :].index)
+
+def plot_predictions(y_test, yhat, start, end):
+    y_test[start: end].plot(label = 'Test Values')
+    yhat[start: end].plot(label = 'Predictions')
+    #plt.plot(inv_y[-100:], label='Test Values')
+    #plt.plot(inv_yhat[-100:], label='Predictions')
+    plt.title(target + ' predictions in ' + loc_target)
+    plt.legend()
+    plt.show()
+
+
+plot_predictions(y_test, yhat, plot_start, plot_end)
